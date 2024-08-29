5e747aa8cad72d6ed2321b0dcbc23da94801c688
Akrombek <gaybulloyevakrom964@gmail.com>
2024-08-29 17:26:13 +0500

    for5

---
+++ for5/script.js
@@ -0,0 +1,14 @@
+function chiqar() {
+    let ogirligi = prompt()
+    let narxi = prompt("mahsulot narxini kiriting")
+
+    for (let i = 1; i <= 9; i++) {
+        let ogirlik = i / 10
+        let xarajat = ogirlik * narxi * ogirligi
+
+        console.log(i + " kg konfet narxi " + xarajat + "so'm")
+    }
+}
+
+
+chiqar()
